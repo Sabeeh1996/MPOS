5c19925707ae83e28be466689b5bc7201ec33c0f
Muhammad Sabeeh <31490435+Sabeeh1996@users.noreply.github.com>
2021-01-19 22:53:25 +0500

    1

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::resource('products','ProductController');
+
 
 Auth::routes();
-
+Route::resource('products','App\Http\Controllers\ProductController');
+Route::resource('suppliers','App\Http\Controllers\SupplierController');
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
